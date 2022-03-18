fb3d9f51692f8ed66211b3d2b0bc9203bd41d631
Ramsensei <ramsensei@estudiantec.cr>
2022-03-18 14:42:38 -0600

    animation reversed

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+let open = new Map();
+open.set("desing-h3", 0);
+open.set("construct-h3", 0);
+open.set("civil-h3", 0);
+open.set("remodelations-h3", 0);
+open.set("about-h3", 0);
+
 function myFunction(idh3, idp) {
-    let e = document.getElementById(idh3);
-    e.style.animation = 'showContent 2s';
-    e.style.transform = 'translateY(0%)';
-    let p = document.getElementById(idp);
-    p.style.animation = 'showContentp 2s';
-    p.style.opacity = '1';
+        let e = document.getElementById(idh3);
+        let p = document.getElementById(idp);
+        e.style.animationPlayState = 'running';
+        p.style.animationPlayState = 'running';
+        setTimeout(function(){
+            e.style.animationPlayState = 'paused';
+            p.style.animationPlayState = 'paused';
+        },2000)
+        
 }
